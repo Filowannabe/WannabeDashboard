4b218e96d3816f93c63ff35422864e8b9ec93472
ape <ape@localhost.localdomain>
2023-06-14 23:24:11 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import TopBar from "./scenes/global/TopBar";
+import { ColorModeContext, useMode } from "./theme";
+
+function App() {
+  const { theme, colorMode } = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;

405364316e94ea0a046caeb8795db2f3d940f9f7
Filowannabe <ccfelipe97@gmail.com>
2023-06-19 19:59:19 -0500

    Setting up all charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+import Bar from "./scenes/bar";
 import Calendar from "./scenes/calendar";
 import Contacts from "./scenes/contacts";
 import Dashboard from "./scenes/dashboard";
 import Faq from "./scenes/faq";
+import Geography from "./scenes/geography";
 import SideBar from "./scenes/global/SideBar";
 import TopBar from "./scenes/global/TopBar";
 import Invoices from "./scenes/invoices";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie/index";
 import ProfileForm from "./scenes/profileForm";
 import Team from "./scenes/team";
 import { ColorModeContext, useMode } from "./theme";
@@ -30,6 +34,10 @@ function App() {
               <Route path="/form" element={<ProfileForm />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<Faq />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>

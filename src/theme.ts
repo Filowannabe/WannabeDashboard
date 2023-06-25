17cf2852426ea459974d4d91b72e36fee9e0a21b
Filowannabe <ccfelipe97@gmail.com>
2023-06-25 17:25:33 -0500

    Updating typography and color types for themes

--- src/theme.ts
+++ src/theme.ts
@@ -165,45 +165,48 @@ export const themeSettings = (mode: string) => {
           }),
     },
     typography: {
-      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
       fontSize: 12,
       h1: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
         fontSize: 40,
       },
       h2: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
         fontSize: 32,
       },
       h3: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
         fontSize: 24,
       },
       h4: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
         fontSize: 20,
       },
       h5: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
         fontSize: 16,
       },
       h6: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "Source Sans Pro", "sans-serif"].join(","),
         fontSize: 14,
       },
     },
   };
 };
-
 //context for the color mode
-export const ColorModeContext = createContext({
-  toggleColorMode: () => {},
-});
+type ColorModelType = {
+  toggleColorMode: (value: React.SetStateAction<string>) => void;
+};
+
+const colorModel: ColorModelType = { toggleColorMode: () => {} };
+
+export const ColorModeContext = createContext(colorModel);
 
 export const useMode = () => {
   const [mode, setMode] = useState<string>("dark");
 
-  const colorMode = useMemo(
+  const colorMode: ColorModelType = useMemo(
     () => ({
       toggleColorMode: () =>
         setMode((prev: string) => (prev === "light" ? "dark" : "light")),
